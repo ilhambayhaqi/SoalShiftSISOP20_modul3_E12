7af67ec3f657b63f2f1aab21b223bb9aa3733a95
ilhambayhaqi <57692117+ilhambayhaqi@users.noreply.github.com>
2020-04-11 17:19:48 +0700

    Revisi no 2

--- soal2/tapplayer.c
+++ soal2/tapplayer.c
@@ -7,16 +7,55 @@
 #include <arpa/inet.h>
 #include <stdbool.h>
 #include <string.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <assert.h>
+#include <pthread.h>
 #define PORT 8080
 
 typedef struct Auth{
 	char username[256];
 	char password[256];
 }Auth;
+
+struct termios org_opts, new_opts;
+char status[256];
+int sock = 0, valread;
+
+bool SetSocketBlockingEnabled(int fd, bool blocking)
+{
+    if (fd < 0) return false;
+
+   int flags = fcntl(fd, F_GETFL, 0);
+   if (flags == -1) return false;
+   flags = blocking ? (flags & ~O_NONBLOCK) : (flags | O_NONBLOCK);
+   return (fcntl(fd, F_SETFL, flags) == 0) ? true : false;
+}
+
+void setNewAttr(void){
+    int c = 0;
+    int res = 0;
+    res = tcgetattr(STDIN_FILENO, &org_opts);
+    assert(res==0);
+
+    memcpy(&new_opts, &org_opts, sizeof(new_opts));
+    new_opts.c_lflag &= ~(ICANON | ECHO | ECHOE | ECHOK | ECHONL | ECHOPRT | ECHOKE | ICRNL);
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_opts);
+}
+
+void returnAttr(void){
+    int res;
+    res=tcsetattr(STDIN_FILENO, TCSANOW, &org_opts);
+    assert(res==0);
+}
+
+void* readStatus(void*);
   
 int main(int argc, char const *argv[]) {
+    int fd;
+    SetSocketBlockingEnabled(fd, true);
+
 	struct sockaddr_in address;
-	int sock = 0, valread;
 	struct sockaddr_in serv_addr;
         
    	char charbuffer[1024] = {0};
@@ -106,6 +145,9 @@ int main(int argc, char const *argv[]) {
     	if(screen == 2){
             printf("\033c");
             if(!strcmp(auth, "authSuccess")) printf("Login Berhasil\n");
+            if(!strcmp(status, "Win")) printf("Game berakhir Kamu Menang!!\n");
+            else if(!strcmp(status, "Lose")) printf("Game berakhir Kamu Kalah!!\n");
+            memset(status, '\0', sizeof(status));
             strcpy(auth, "Udah login");
 
             printf("1. Find Match\n");
@@ -122,24 +164,58 @@ int main(int argc, char const *argv[]) {
                 strcpy(charbuffer, "findPlayer");
                 send(sock, charbuffer, strlen(charbuffer), 0);
 
-                char status[256];
                 printf("\033c");
                 printf("Waiting for player ...\n");
 
-                memset(status, '\0', sizeof(status));
+                //memset(status, '\0', sizeof(status));
                 read(sock, status, 256);
 
-                while(strcmp(status, "Win") || strcmp(status, "Lose")){
-                    printf("Play\n");
-                    fgets(input,256, stdin);
-                    char *tap = "tap";
-                    send(sock, tap, strlen(tap), 0);
+                printf("Game dimulai silahkan tap tap secepat mungkin !!\n");
+
+                pthread_t tid;
+                pthread_create(&tid, NULL, readStatus, NULL);
+
+                setNewAttr();
+
+                while(!(!strcmp(status, "Win") || !strcmp(status, "Lose"))){
+                    //fgets(input,256, stdin);
+                    SetSocketBlockingEnabled(fd, false);
+                    char c = getchar();
+                    SetSocketBlockingEnabled(fd, true);
                     
-                    memset(status, '\0', sizeof(status));
-                    read(sock, status, 256);
+                    if(c == ' ' && !(!strcmp(status, "Win") || !strcmp(status, "Lose"))){
+                        printf("HIT!!\n");
+                        send(sock, &c, 1, 0);
+                    }
+
+
+                    //else printf("Huhu");
+                    
+                    //memset(status, '\0', sizeof(status));
+                    //read(sock, status, 256);
                 }
-                printf("%s\n", status);       
+                returnAttr();
+
+                //printf("%s\n", status);       
             }
     	}
+    }
+}
+
+void* readStatus(void* argument){
+    char *gameover = "gameover";
+    while(!(!strcmp(status, "Win") || !strcmp(status, "Lose"))){
+        memset(status, '\0', sizeof(status));
+        //printf("%s\n", status);
+        read(sock, status, 256);
+        printf("-Health %s-\n", status);
+        if(!strcmp(status,"Win")){
+            send(sock, gameover, strlen(gameover), 0);
+            break;
+        }
+        if(!strcmp(status, "Lose")){
+            send(sock, gameover, strlen(gameover), 0);
+            break;
+        }
     }
 }
